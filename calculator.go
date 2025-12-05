1ea639b89c491673bbf642acaed30a6c1d566d84
Genny4Real <and.russo.0511@gmail.com>
2025-12-05 01:38:52 +0100

    First Usable Version
    With this version you could do simple operations with the cli app. It will ask user again for input if its invalid or if its not an int in valid cases. Should be almost bug free

--- calculator.go
+++ calculator.go
@@ -3,9 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
-	"unicode"
-	"strconv"
 	"os"
+	"strconv"
+	"unicode"
 )
 
 func main () {
@@ -13,21 +13,58 @@ func main () {
 // init_calc
 	x, err1, y, err2 := init_calc()
 	if err1 != nil || err2 != nil{
-		fmt.Printf("Error with Calculator Initialization. Exiting program...")
+		fmt.Printf("Error with Calculator Initialization. Exiting program...\n")
 		os.Exit(1)
 	}
-	fmt.Printf("%d / %d = %f\n", x, y, float32(x) / float32(y))
-	os.Exit(0)
+
+	for {
+		fmt.Printf("Enter the desired operation.\n")
+		operation:= get_input(false)
+
+		if len(operation) == 1 {
+			switch operation {
+				case "+":
+					result := addition(x, y)
+					fmt.Printf("%d %v %d = %d\n", x, operation, y, result)
+					os.Exit(0)
+				case "-":
+					result := subtraction(x, y)
+					fmt.Printf("%d %v %d = %d\n", x, operation, y, result)
+					os.Exit(0)
+				case "*":
+					result := multiplication(x ,y)
+					fmt.Printf("%d %v %d = %d\n", x, operation, y, result)
+					os.Exit(0)
+				case "/":
+					result := float32(division(x , y))
+					fmt.Printf("%d %v %d = %f\n", x, operation, y, result)
+					os.Exit(0)
+				default:
+					fmt.Printf("Error with operation switching. Exiting program...\n")
+					os.Exit(0)
+			}
+		}
+		fmt.Printf("Invalid input. Please enter one of +, -, *, /\n")
+	}
+
 }
 
-func get_input () (string, error) {
+func get_input (is_int bool) (string) {
 	var x string
-	fmt.Printf("Scanning for input: ")
-	fmt.Scanln(&x)
-	if x == "" {
-		return "", errors.New("empty input")
+	for {
+		fmt.Printf("Scanning for input: ")
+		fmt.Scanln(&x)
+		if x != "" {
+			if is_int == true {
+				if check_if_int(x) == nil {
+					return x
+				}
+				continue
+			}
+			return x
+		}
+		fmt.Printf("Incorrect input. Try again.\n")
 	}
-	return x, nil
 }
 
 func check_if_int (input string) (error) {
@@ -41,34 +78,36 @@ func check_if_int (input string) (error) {
 }
 
 func init_calc () (int, error, int, error) { // need to fix returns
-	var (
-		input1, error1 = get_input()
-		input2, error2 = get_input()
-	)
-	if error1 != nil || error2 != nil {
-		fmt.Printf("Invalid Input. Exiting Program...\n")
-		return 0, errors.New("invalid input"), 0, errors.New("invalid input")
-	}
-
-	if check_if_int(input1) != nil {
-		fmt.Printf("First input not a valid number. Exiting Program...\n")
-		return 0, errors.New("invalid input"), 0, errors.New("invalid input")
-	}
-	if check_if_int(input2) != nil {
-		fmt.Printf("Second input not a valid number. Exiting Program...\n")
-		return 0, errors.New("invalid input"), 0, errors.New("invalid input")
-	}
+	fmt.Printf("Welcome to your Personal Calculator! Input the two operands!\n")
+	input1 := get_input(true)
+	input2 := get_input(true)
 
 	x, error3 := strconv.Atoi(input1)
 	if error3 != nil {
-		fmt.Printf("Error with first input conversion to integer value. Exiting Program...")
+		fmt.Printf("Error with first input conversion to integer value. Exiting Program...\n")
 		return 0, errors.New("invalid input"), 0, errors.New("invalid input")
 	}
 
 	y, error4 := strconv.Atoi(input2)
 	if error4 != nil {
-		fmt.Printf("Error with first input conversion to integer value. Exiting Program...")
+		fmt.Printf("Error with first input conversion to integer value. Exiting Program...\n")
 		return 0, errors.New("invalid input"), 0, errors.New("invalid input")
 	}
 	return x, nil, y, nil
+}
+
+func addition (a int, b int) (int) {
+	return a+b
+}
+
+func subtraction (a int, b int) (int) {
+	return a-b
+}
+
+func multiplication (a int, b int) (int) {
+	return a*b
+}
+
+func division (a int, b int) (float32) {
+	return float32(a)/float32(b)
 }
