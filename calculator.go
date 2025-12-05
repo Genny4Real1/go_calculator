78308db3e844280a783ee6cadb253d043bd0d401
Genny4Real <and.russo.0511@gmail.com>
2025-12-05 02:13:17 +0100

    Updated code to match Standard
    Made code clearer and more adherent to standard. Also fixed the following bugs
    
    Impossible to input negative numbers
    
    Division by zero giving inf instead of treating it as undefined

--- calculator.go
+++ calculator.go
@@ -1,113 +1,85 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
-	"unicode"
 )
 
-func main () {
-
-// init_calc
-	x, err1, y, err2 := init_calc()
-	if err1 != nil || err2 != nil{
-		fmt.Printf("Error with Calculator Initialization. Exiting program...\n")
-		os.Exit(1)
-	}
+func main() {
+	fmt.Printf("Welcome to your Personal Calculator! Input the two operands!\n")
+	x := getNumber()
+	y:= getNumber()
 
 	for {
 		fmt.Printf("Enter the desired operation.\n")
-		operation:= get_input(false)
+		operation:= getString()
 
-		if len(operation) == 1 {
+		if len(operation) == 1 && operation == "+" || operation == "-" || operation == "*" || operation == "/"{
 			switch operation {
 				case "+":
 					result := addition(x, y)
 					fmt.Printf("%d %v %d = %d\n", x, operation, y, result)
-					os.Exit(0)
 				case "-":
 					result := subtraction(x, y)
 					fmt.Printf("%d %v %d = %d\n", x, operation, y, result)
-					os.Exit(0)
 				case "*":
 					result := multiplication(x ,y)
 					fmt.Printf("%d %v %d = %d\n", x, operation, y, result)
-					os.Exit(0)
 				case "/":
-					result := float32(division(x , y))
-					fmt.Printf("%d %v %d = %f\n", x, operation, y, result)
-					os.Exit(0)
-				default:
-					fmt.Printf("Error with operation switching. Exiting program...\n")
-					os.Exit(0)
+
+					if y != 0 {
+						result := division(x , y)
+						fmt.Printf("%d %v %d = %.2f\n", x, operation, y, result)
+					}
+					fmt.Printf("Division by 0 is undefined.")
 			}
+			break
 		}
 		fmt.Printf("Invalid input. Please enter one of +, -, *, /\n")
 	}
-
+	os.Exit(0)
 }
 
-func get_input (is_int bool) (string) {
+func getString () string {
 	var x string
 	for {
 		fmt.Printf("Scanning for input: ")
 		fmt.Scanln(&x)
 		if x != "" {
-			if is_int == true {
-				if check_if_int(x) == nil {
-					return x
-				}
-				continue
-			}
 			return x
 		}
 		fmt.Printf("Incorrect input. Try again.\n")
 	}
 }
 
-func check_if_int (input string) (error) {
-	for _, char := range input {
-		if unicode.IsDigit(char) == false {
-			return errors.New("not valid number")
+func getNumber () int {
+	var x string
+	for {
+		fmt.Printf("Scanning for input: ")
+		fmt.Scanln(&x)
+		if x != "" {
+			xAtoi, err := strconv.Atoi(x)
+			if err == nil {
+				return xAtoi
+			}
 		}
-
+		fmt.Printf("Incorrect input. Try again.\n")
 	}
-	return nil
 }
 
-func init_calc () (int, error, int, error) { // need to fix returns
-	fmt.Printf("Welcome to your Personal Calculator! Input the two operands!\n")
-	input1 := get_input(true)
-	input2 := get_input(true)
-
-	x, error3 := strconv.Atoi(input1)
-	if error3 != nil {
-		fmt.Printf("Error with first input conversion to integer value. Exiting Program...\n")
-		return 0, errors.New("invalid input"), 0, errors.New("invalid input")
-	}
-
-	y, error4 := strconv.Atoi(input2)
-	if error4 != nil {
-		fmt.Printf("Error with first input conversion to integer value. Exiting Program...\n")
-		return 0, errors.New("invalid input"), 0, errors.New("invalid input")
-	}
-	return x, nil, y, nil
-}
-
-func addition (a int, b int) (int) {
+func addition (a int, b int) int {
 	return a+b
 }
 
-func subtraction (a int, b int) (int) {
+func subtraction (a int, b int) int {
 	return a-b
 }
 
-func multiplication (a int, b int) (int) {
+func multiplication (a int, b int) int {
 	return a*b
 }
 
-func division (a int, b int) (float32) {
+func division (a int, b int) float32 {
 	return float32(a)/float32(b)
 }
